09b87e417fce46a86178f6de6e9432d855e34d73
DevAngelo98 <ainglima07@gmail.com>
2019-10-17 20:13:57 +0200

    upgrade

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,24 +1,27 @@
 function change(){
   document.getElementById("first_compra").style.display = "none";
+  document.getElementById("padd").style.paddingTop = "20px";
   document.getElementById("all_ticket").style.display = "flex";
 }
 
 function generator(){
   var chilometri = parseInt(document.getElementById("form_chilometri").value);
   var eta = parseInt(document.getElementById("form_eta").value);
-  console.log(chilometri);
-  console.log(eta);
+  
   var prezzo = 0.21 * chilometri;
 
   if(eta<18){
     var prezzoFinale = prezzo - ((prezzo * 20)/100);
+    document.getElementById("form_sconto").innerHTML = "20% perché minorenne";
   } else if(eta > 65){
     var prezzoFinale = prezzo - ((prezzo * 40)/100);
+    document.getElementById("form_sconto").innerHTML = "40% perché over 65";
   } else if(eta >= 18 && eta <= 65){
     var prezzoFinale = prezzo;
+    document.getElementById("form_sconto").innerHTML = "0%";
   }
 
   console.log(prezzoFinale);
   document.getElementById("form_prezzo").innerHTML = prezzoFinale+" €";
-
+  
 }
