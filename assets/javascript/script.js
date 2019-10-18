0f441bd453cd1f091fdfcf0dcbcd557e230f7914
DevAngelo98 <ainglima07@gmail.com>
2019-10-18 17:24:12 +0200

    Controlli completi, reset campi in caso di esito negativo

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,16 +5,16 @@ function change(){
   document.getElementById("all_ticket").style.display = "flex";
 }
 
-
+// Funzione per verificare se non è stato inserito un numero
 function verificaValori(numero){
-  if (isNaN(numero)){
-    alert("Non ha inserito correttamente i valori, riprovi");
+  if (isNaN(numero) || numero === ""){
     return false;
   }
+  return true;
 }
 
 // Funzione generatrice prezzo del biglietto in base a chilometri ed eta
-function generator(){
+function generator() {
 
   // Dichiaro tutte le variabili
   var chilometri = document.getElementById("form_chilometri").value;
@@ -22,14 +22,14 @@ function generator(){
   var prezzo = 0.21 * chilometri;
   var prezzoFinale;
 
-  // Controllo dei parametri inseriti
-  verificaValori(chilometri);
-  verificaValori(eta);
-
-
-  // Condizioni per stabilire il prezzo finale
-  
-  if (eta<18) {
+  // Controllo dei parametri inseriti; in caso negativo reset dei campi
+  if(verificaValori(chilometri) == false || verificaValori(eta) == false ){
+    alert("Non ha inserito correttamente i valori, riprovi");
+    document.getElementById("form_chilometri").value = null;
+    document.getElementById("form_eta").value = null;
+    document.getElementById("form_sconto").innerHTML = "";
+    document.getElementById("form_prezzo").innerHTML = "";
+  } else if (eta  < 18 ) {
     prezzoFinale = prezzo - ((prezzo * 20)/100);
     document.getElementById("form_sconto").innerHTML = "20% perché minorenne";
   } else if (eta > 65) {
@@ -40,7 +40,6 @@ function generator(){
     document.getElementById("form_sconto").innerHTML = "0%";
   }
 
-  // console.log(prezzoFinale);
-  document.getElementById("form_prezzo").innerHTML = prezzoFinale+" €";
+  document.getElementById("form_prezzo").innerHTML = prezzoFinale.toFixed(2)+" €";
   
 }
